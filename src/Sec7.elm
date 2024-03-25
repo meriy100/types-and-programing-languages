65fce03edb59e28fc18b885b68b49026316cdc71
shun0918 <nihei@tebiki.co.jp>
2024-03-25 17:59:59 +0900

    tmp: 7章

--- src/Sec7.elm
+++ src/Sec7.elm
@@ -3,13 +3,94 @@ module Sec7 exposing (..)
 import Html as H
 
 
+type Info
+    = Info
+
+
+type Binding
+    = NameBind
+    | None
+
+
+type alias Context =
+    List ( String, Binding )
+
+
 type Term
-    = None
+    = TmVar Info Int Int
+    | TmAbs Info String Term
+    | TmApp Info Term Term
+
+
+pickfreshname : Context -> String -> ( Context, String )
+pickfreshname ctx x =
+    if List.member x (List.map Tuple.first ctx) then
+        pickfreshname ctx (x ++ "'")
+
+    else
+        ( ( x, NameBind ) :: ctx
+        , x
+        )
+
+
+printtm : Context -> Term -> String
+printtm ctx t =
+    case t of
+        TmAbs fi x t1 ->
+            let
+                ( ctx1, x1 ) =
+                    pickfreshname ctx x
+            in
+            "(lambda " ++ x1 ++ ". " ++ printtm ctx1 t1 ++ ")"
+
+        TmApp fi t1 t2 ->
+            "(" ++ printtm ctx t1 ++ " " ++ printtm ctx t2 ++ ")"
+
+        TmVar fi x n ->
+            if List.length ctx == n then
+                index2name fi ctx x
+
+            else
+                "[bad index]"
+
+
+index2name : Info -> Context -> Int -> String
+index2name fi ctx x =
+    let
+        _ =
+            Debug.log "index2name" ( x, ctx )
+
+        ( _, b ) =
+            List.drop (x - 1) ctx
+                |> List.head
+                |> Maybe.withDefault ( "[index2name error1]", None )
+    in
+    case b of
+        NameBind ->
+            let
+                ( n, _ ) =
+                    List.drop (x - 1) ctx
+                        |> List.head
+                        |> Maybe.withDefault ( "[index2name error2]", None )
+            in
+            n
+
+        _ ->
+            "[index2name error3]"
 
 
 source : Term
+
+
+
+--source =
+--    TmAbs Info "s" (TmAbs Info "z" (TmVar Info 0 2))
+
+
 source =
-    None
+    TmApp Info
+        (TmAbs Info "s" (TmAbs Info "z" (TmVar Info 1 2)))
+        (TmApp Info (TmVar Info 1 2) (TmVar Info 0 2))
 
 
 main =
@@ -17,6 +98,7 @@ main =
         [ H.div []
             [ source |> Debug.toString |> H.text |> List.singleton |> H.p []
             , source
+                |> printtm []
                 |> Debug.toString
                 |> H.text
                 |> List.singleton
