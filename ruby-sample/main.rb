54b73bbed5dad37d589a4dc212b0489067a5f594
meriy100 <ttattataa@gmail.com>
2024-05-20 21:33:36 +0900

    fix rubocop

--- ruby-sample/main.rb
+++ ruby-sample/main.rb
@@ -1,67 +1,64 @@
+# frozen_string_literal: true
+
 # ZERO = ->(s) { -> (z) { z } }
 # ONE =  ->(s) { ->(z) { s[z] } }
 
-
 def is_numeric_val(t)
   case t
-  in "TmZero"
+  in 'TmZero'
     true
-  in "TmSucc", t1
+  in 'TmSucc', t1
     is_numeric_val(t1)
   else
     false
   end
 end
 
-TyBool = "Bool"
-TyNat = "Nat"
+TyBool = 'Bool'
+TyNat = 'Nat'
 
 def typeof(ctx, t)
   case t
-  in "TmTrue"
+  in 'TmTrue', _
     TyBool
-  in "TmFalse"
+  in 'TmFalse', _
     TyBool
-  in "TmIf", t1, t2, t3
-    if typeof(ctx, t1) == TyBool && typeof(ctx, t2) == typeof(ctx, t3)
-      typeof(ctx, t2)
-    else
-      raise "Type Error"
-    end
-  in "TmZero"
+  in 'TmIf', _, t1, t2, t3
+    raise 'Type Error' unless typeof(ctx, t1) == TyBool && typeof(ctx, t2) == typeof(ctx, t3)
+
+    typeof(ctx, t2)
+
+  in 'TmZero', _
     TyNat
-  in "TmSucc", t1
-    if typeof(ctx, t1) == TyNat
-      TyNat
-    else
-      raise "Type Error"
-    end
-  in "TmPred", t1
-    if typeof(ctx, t1) == TyNat
-      TyNat
-    else
-      raise "Type Error"
-    end
-  in "TmIsZero", t1
-    if typeof(ctx, t1) == TyNat
-      TyBool
-    else
-      raise "Type Error"
-    end
+  in 'TmSucc', _, t1
+    raise 'Type Error' unless typeof(ctx, t1) == TyNat
+
+    TyNat
+
+  in 'TmPred', _, t1
+    raise 'Type Error' unless typeof(ctx, t1) == TyNat
+
+    TyNat
+
+  in 'TmIsZero', _, t1
+    raise 'Type Error' unless typeof(ctx, t1) == TyNat
+
+    TyBool
+
   else
-    raise "No Type"
+    raise "No Type #{t}"
   end
 end
 
 def is_val(t)
   case t
-  in "TmTrue"
+  in 'TmTrue', _
     true
-  in "TmFalse"
+  in 'TmFalse', _
     true
-  in "TmZero"
+  in 'TmZero', _
     true
-  in "TmSucc", t1
+  in 'TmSucc', _, t1
     is_numeric_val(t1)
   else
     false
@@ -70,51 +67,48 @@ end
 
 def eval1(t)
   case t
-  in "TmIf", ["TmTrue"], t2, t3
+  in 'TmIf', _, ['TmTrue', _], t2, t3
     t2
-  in "TmIf", ["TmFalse"], t2, t3
+  in 'TmIf', _, ['TmFalse', _], t2, t3
     t3
-  in "TmIf", t1, t2, t3
+  in 'TmIf', _, t1, t2, t3
     t1_ = eval1(t1)
-    ["TmIf", t1_, t2, t3]
-  in "TmSucc", t1
+    ['TmIf', _, t1_, t2, t3]
+  in 'TmSucc', _, t1
     result = eval1(t1)
-    ["TmSucc", result]
-  in "TmPred", ["TmZero"]
-    ["TmZero"]
-  in "TmPred", ["TmSucc", nv]
-    if is_numeric_val(nv)
-      nv
-    else
-      raise "No Apply Rule"
-    end
-  in "TmIsZero", ["TmZero"]
-    ["TmTrue"]
-  in "TmIsZero", ["TmSucc", nv]
-    if is_numeric_val(nv)
-      ["TmFalse"]
-    else
-      raise "No Apply Rule"
-    end
-  in "TmIsZero", t1
+    ['TmSucc', _, result]
+  in 'TmPred', _, ['TmZero', _]
+    ['TmZero', _]
+  in 'TmPred', _, ['TmSucc', _, nv]
+    raise 'No Apply Rule' unless is_numeric_val(nv)
+
+    nv
+
+  in 'TmIsZero', _, ['TmZero', _]
+    ['TmTrue', _]
+  in 'TmIsZero', _, ['TmSucc', _, nv]
+    raise 'No Apply Rule' unless is_numeric_val(nv)
+
+    ['TmFalse', _]
+
+  in 'TmIsZero', _, t1
     t1_ = eval1(t1)
-    ["TmIsZero", t1_]
-  in "TmSucc", t1
+    ['TmIsZero', _, t1_]
+  in 'TmSucc', _, t1
     t1_ = eval1(t1)
-    ["TmIsZero", t1_]
+    ['TmIsZero', _, t1_]
   else
-    raise "No Apply Rule"
+    raise 'No Apply Rule'
   end
 end
 
-ZERO = ["TmZero"]
-ONE = ["TmSucc", ["TmZero"]]
-IF = -> (b) { -> (t) { ->(f) { ["TmIf", b, t, f] } } }
-
+ZERO = ['TmZero', nil].freeze
+ONE = ['TmSucc', nil, ['TmZero', nil]].freeze
+IF = ->(b) { ->(t) { ->(f) { ['TmIf', nil, b, t, f] } } }
 
 def main
   source =
-    IF[["TmFalse"]][
+    IF[['TmFalse', nil]][
       ZERO
     ][
       ONE
